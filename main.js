f03b1e1d4e56a39e261d595a5764b0b3fc078f98
s1rene <71638008+s1rene@users.noreply.github.com>
2025-03-29 11:07:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
